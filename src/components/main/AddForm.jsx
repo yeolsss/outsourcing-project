635f412aa2e2d2de728758179b1ec9a3bb05fe06
Hamin Lee <whereanna7@gmail.com>
2023-12-05 18:18:05 +0900

    [add]AddForm 컴포넌트 추가 및 등록모드 state 추가

---
+++ src/components/main/AddForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddForm() {
+  return (
+    <div>투게더 등록하기</div>
+  )
+}
+
+export default AddForm
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddForm from '../components/main/AddForm';
 
 function Home() {
-  return <div>Home</div>;
+  const [isAdding, setIsAdding] = useState(false);
+
+  return (
+  <>
+    <div>
+      {isAdding? (
+        <AddForm />
+      ) : (
+        <div>리스트영역</div>
+      )}
+
+    </div>
+    <div>
+      지도영역
+    </div>
+  </>)
 }
 
 export default Home;
