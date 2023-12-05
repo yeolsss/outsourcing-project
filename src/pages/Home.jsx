0f335f303b7b0b217b2ee27ddf5b2590a79e6b61
Hamin Lee <whereanna7@gmail.com>
2023-12-05 18:36:18 +0900

    [add] 새글등록 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
+import { styled } from 'styled-components';
 import AddForm from '../components/main/AddForm';
 
 function Home() {
   const [isAdding, setIsAdding] = useState(false);
 
   return (
-  <>
-    <div>
+  <StMainContainer>
+    <AddBtn onClick={()=> setIsAdding(!isAdding)}>새 게더 등록</AddBtn>
+    <StLeftContainer>
       {isAdding? (
         <AddForm />
       ) : (
         <div>리스트영역</div>
       )}
 
-    </div>
+    </StLeftContainer>
     <div>
       지도영역
     </div>
-  </>)
+  </StMainContainer>)
 }
 
 export default Home;
+
+const AddBtn = styled.button`
+background-color: yellow;
+border: 1px solid black;
+`;
+
+const StMainContainer = styled.div`
+  background-color: pink;
+  display: flex;
+  flex-direction: row;
+`;
+
+const StLeftContainer = styled.div`
+  background-color: lightskyblue;
+  width: 50%;
+`;
