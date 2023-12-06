50a65a87df9f86210e53af7834515ecbcb215e15
eiansd <joh7727@naver,com>
2023-12-06 10:46:03 +0900

    머지

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -1,4 +1,7 @@
 import {
+  getAnalytics
+} from "firebase/analytics";
+import {
   initializeApp
 } from "firebase/app";
 
@@ -11,40 +14,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APPID,
 };
 
-const ap // Import the functions you need from the SDKs you need
-  <<
-  << << < HEAD ===
-  === =
-  import {
-    getAnalytics
-  } from "firebase/analytics";
-import {
-  initializeApp
-} from "firebase/app"; >>>
->>> > ea707298b4b6a1a95e40bcc28127fb4bbda096bc
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  <<
-  << << < HEAD ===
-  === =
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  >>>
-  >>> > ea707298b4b6a1a95e40bcc28127fb4bbda096bc
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-};
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); <<
-<< << < HEAD
-  ===
-  === =
-  const analytics = getAnalytics(app); >>>
->>> > ea707298b4b6a1a95e40bcc28127fb4bbda096bc
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
