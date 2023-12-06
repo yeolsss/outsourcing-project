c94c9afc80d2badc09c7197ea6f123dec3f2d86a
yeolsss <yeol8810@gmail.com>
2023-12-06 11:28:09 +0900

    [fix] 임시저장

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
